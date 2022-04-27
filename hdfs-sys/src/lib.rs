b70cc4233212540246b9d1f5e150bc41462290ea
Xuanwo <github@xuanwo.io>
2022-04-27 14:51:42 +0800

    refactor: Split hdfs-sys to another repo  (#4)
    * Vendored can't works for hdfs
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * Save work
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * Remove vendors
    
    Signed-off-by: Xuanwo <github@xuanwo.io>
    
    * refactor: Split hdfs-sys to another repo
    
    Signed-off-by: Xuanwo <github@xuanwo.io>

--- hdfs-sys/src/lib.rs
+++
@@ -1,8 +0,0 @@
-#![allow(non_upper_case_globals)]
-#![allow(non_camel_case_types)]
-#![allow(non_snake_case)]
-#![allow(clippy::redundant_static_lifetimes)]
-#![allow(deref_nullptr)]
-#![allow(rustdoc::invalid_rust_codeblocks)]
-
-include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
